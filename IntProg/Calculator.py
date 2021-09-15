78494746c359223a792b0c06ca1ad25723e08c7e
den-duar <2021211510079@iesp.edu.br>
2021-09-15 02:36:34 -0300

    Basic if/elif test using a calculator

--- IntProg/Calculator.py
+++ IntProg/Calculator.py
@@ -1,3 +1,16 @@
+print("Digite a operação que deseja")
+print("1 - adição")
+print ("2 - subtração")
+print ("3 - divisão")
+print ("4 - multiplicação")
+operation = (input("Escolha a operação:"))
 n1 = float(input("1 Numero: "))
 n2 = float(input("2 Numero: "))
-print (n1+n2)
+if operation == "1":
+    print (n1+n2)
+elif operation == "2":
+    print (n1-n2)
+elif operation == "3":
+    print (n1/n2)
+elif operation == "4":
+    print (n1*n2)
