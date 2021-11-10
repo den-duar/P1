128e0541bb8c62da37cd97538eea3163a7c84d1e
den-duar <2021211510079@iesp.edu.br>
2021-11-09 21:11:17 -0300

    list functions and multiple type elements testing

---
+++ IntProg/ListTest.py
@@ -0,0 +1,6 @@
+list = [0, '0']
+print(type(list))
+print(type(list[0]))
+print(type(list[1]))
+list.append("inserted") #insert to put element into a specific position list.insert(X,insertion)
+print (list)
